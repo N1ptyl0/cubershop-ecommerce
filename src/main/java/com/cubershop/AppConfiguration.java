65826cb42ba8a9ce5196dab7178897af5549a3d1
neemiassgc <neemiassgc@outlook.com>
2021-05-31 18:41:24 -0300

    remove configuration bean 'prodHikariDataSource'

--- src/main/java/com/cubershop/AppConfiguration.java
+++ src/main/java/com/cubershop/AppConfiguration.java
@@ -1,12 +1,10 @@
 package com.cubershop;
 
-import com.cubershop.converter.*;
+import com.cubershop.converter.DoubleToPriceConverter;
+import com.cubershop.converter.PriceFormatter;
+import com.cubershop.converter.StringToEnumConverter;
 import com.cubershop.validation.CubeValidation;
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.validation.Validator;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -24,20 +22,5 @@ public class AppConfiguration implements WebMvcConfigurer {
     @Override
     public Validator getValidator() {
         return new CubeValidation();
-    }
-
-    @Bean
-    @Profile("prod")
-    public HikariDataSource prodHikariDataSource() {
-        HikariConfig config = new HikariConfig() {
-            {
-                setDriverClassName("org.postgresql.Driver");
-                setJdbcUrl(System.getenv("URL"));
-                setUsername(System.getenv("SPRING_DATASOURCE_USERNAME"));
-                setPassword(System.getenv("SPRING_DATASOURCE_PASSWORD"));
-            }
-        };
-
-        return new HikariDataSource(config);
     }
 }
