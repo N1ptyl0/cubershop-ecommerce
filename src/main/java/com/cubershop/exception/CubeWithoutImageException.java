dce1f7d74085eb8f231eedf0c6a071c2b9b2be27
neemiassgc <neemiassgc@outlook.com>
2021-06-09 10:44:17 -0300

    Add CubeWithoutImageException runtime exception

---
+++ src/main/java/com/cubershop/exception/CubeWithoutImageException.java
@@ -0,0 +1,8 @@
+package com.cubershop.exception;
+
+public final class CubeWithoutImageException extends RuntimeException {
+
+	public CubeWithoutImageException(final String message) {
+		super(message);
+	}
+}
